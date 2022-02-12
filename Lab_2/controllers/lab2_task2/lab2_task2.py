e0ab95e145de485fb36067c82227a192e8fff25d
steven salinas <stevensalinas@live.com>
2022-02-12 17:11:28 -0500

    Adding basic given files Lab_2

---
+++ Lab_2/controllers/lab2_task2/lab2_task2.py
@@ -0,0 +1,54 @@
+"""lab2_task2 controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getMotor('motorname')
+#  ds = robot.getDistanceSensor('dsname')
+#  ds.enable(timestep)
+
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+leftMotor.setVelocity(0)
+rightMotor.setVelocity(0)
+
+#getting the position sensors
+
+leftposition_sensor = robot.getDevice('left wheel sensor')
+rightposition_sensor = robot.getDevice('right wheel sensor')
+leftposition_sensor.enable(timestep)
+rightposition_sensor.enable(timestep)
+
+imu = robot.getDevice('inertial unit')
+imu.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+    leftMotor.setVelocity(6.28)
+    rightMotor.setVelocity(-6.28)
+    
+    print("Left position sensor: " +str(leftposition_sensor.getValue()))
+    print("Right position sensor: " +str(rightposition_sensor.getValue()))
+    # Process sensor data here.
+    print("imu: "+str(imu.getRollPitchYaw()[2]))
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
+
+# Enter here exit cleanup code.
