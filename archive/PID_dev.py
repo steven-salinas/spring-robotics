57e2c0b1b563648d5dc5dbf2f2e6f4adde25d8b4
Yehia-Helwa <yehiahelwa@gmail.com>
2022-02-15 00:22:05 -0500

    Fixed PID

--- archive/PID_dev.py
+++ archive/PID_dev.py
@@ -10,14 +10,21 @@ initPID()
 # P=0.02
 # I=0.01
 # D=0
+
 # pidL = PID.PID(P,I,D)
 # pidR = PID.PID(P,I,D)
 # pidL.setSampleTime(0.01)
 # pidR.setSampleTime(0.01)
 speedL=1
 speedR=1
+print("hi")
+setSpeedsIPS(1,1)
 
-setSpeedsIPS(3,3)
+second_timer=time.time()+1
+while True:
+    if time.time()>second_timer:
+        second_timer=time.time()+1
+        print(getSpeeds())
 time.sleep(10000)
 # current_speedL=speedL
 # current_speedR=speedR
