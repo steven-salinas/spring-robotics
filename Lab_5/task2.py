512310352541edc5f4277b92fc17b2ec5d238783
steven salinas <stevensalinas@live.com>
2022-04-22 18:32:26 -0400

    Corrected sensor update

--- Lab_5/task2.py
+++ Lab_5/task2.py
@@ -1,7 +1,8 @@
 import copy
-import utils
 import time
 
+'''
+import utils
 import signal
 import math
 import RPi.GPIO as GPIO # README: https://pypi.org/project/RPi.GPIO/
@@ -24,6 +25,7 @@ def ctrlC(signum, frame):
     utils.setSpeedsPWM (1.508,1.5)
     GPIO.cleanup()
     exit()
+'''
 
 directions = ['W', 'N', 'E', 'S']
 cell_to_index = {1:(0,0),2:(0,1),3:(0,2),4:(0,3),
@@ -46,52 +48,113 @@ single_square = {2:['N','S'],3:['N'],4:['W'],5:['S'],6:['S'],7:['E','W'],8:['E',
                 ,9:['E','W'],10:['E'],11:['W'],12:['E','W']
                 ,13:['E'],14:['N','S'],15:['N','S'],16:['W']}
 
+# Map of walls based on cell number and facing north
+grid_map = {1:'WWOW', 2:'OWOW', 3:'OWOO', 4:'OWWO',
+            5:'WWOO', 6:'OWWO', 7:'WOWO', 8:'WOWO',
+            9:'WOWO', 10:'WOOW', 11:'OOWW', 12:'WOWO',
+            13:'WOOW', 14:'OWOW', 15:'OWOW', 16:'OOWW'}
+
 # Motion Model
-prob_forward = 0.7
+prob_forward = 0.6
 prob_left = 0.1
 prob_right = 0.1
 prob_stay = 0.2
 
+# Sensor Model
+prob_z0_s0 = 0.7
+prob_z0_s1 = 0.1
+prob_z1_s0 = 0.3
+prob_z1_s1 = 0.9
+
+# 0 = west, 1 = north, 2 = east, 3 = south
+CURRENT_DIRECTION = 1
+
 # Size of grid square in inches
 SQ_SIZE = 18
+
+# Number of beginning particles
+PARTICLE_NUM = 112
+PARTICLE_THRESHOLD = 0.9 * PARTICLE_NUM
 
 ROWS = 4
 COLUMNS = 4
 
-def sensor_update(left:int,front:int,right:int,input_arr:list[list[int]],dir:str) -> list[list[int]]:
-    reading = [left,front,right]
-    selection = None
-    if reading == [1,1,1]:
-        selection = surrounded_squares
-    elif reading == [1,1,0] or reading == [0,1,1]:
-        selection = adjacent_squares
-    elif reading == [1,0,1]:
-        selection = opposite_squares
-    elif reading == [1,0,0] or reading == [0,1,0] or reading == [0,0,1]:
-        selection = single_square
-    else:
-        selection = None
-
-    belief_arr = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-    cell_prob = 1/len(selection.keys())
-    for key in selection.keys():        
-        i,j = cell_to_index[key]
-        if dir in selection[key]:
-            belief_arr[i][j] = cell_prob * input_arr[i][j]
-
+def sensor_update(readings,input_arr:list[list[int]]) -> list[list[int]]:
+    left_reading, front_reading, right_reading = readings[0],readings[1], readings[2]
+    arr = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
     arr_sum = 0
-    for row in belief_arr:
-        arr_sum += sum(row)
-    
-    # Normalize
-    for row in range(len(belief_arr)):
-        for column in range(len(belief_arr[0])):
-            if belief_arr[row][column] != 0:
-                belief_arr[row][column] = belief_arr[row][column] / arr_sum
-    
-    for row in belief_arr:
+    #print(left_reading,front_reading,right_reading)
+    for row in range(ROWS):
+        for col in range(COLUMNS):
+            cell = index_to_cell[row,col]
+            left_prob, front_prob, right_prob = None, None, None
+            map_left, map_front, map_right = None, None, None
+
+            # Calculations for left of current cell
+            if left_reading == 1:
+                map_left = grid_map[cell][(CURRENT_DIRECTION-1)%4]
+                if map_left == 'W':
+                    left_prob = prob_z1_s1
+                else:
+                    left_prob = prob_z1_s0
+            elif left_reading == 0:
+                map_left = grid_map[cell][(CURRENT_DIRECTION-1)%4]
+                if map_left == 'O':
+                    left_prob = prob_z0_s0
+                else:
+                    left_prob = prob_z0_s1
+
+            # Calculations for front of current cell  
+            if front_reading == 1:
+                map_front = grid_map[cell][CURRENT_DIRECTION]
+                if map_front == 'W':
+                    front_prob = prob_z1_s1
+                else:
+                    front_prob = prob_z1_s0
+            elif front_reading == 0:
+                map_front = grid_map[cell][CURRENT_DIRECTION]
+                if map_front == 'O':
+                    front_prob = prob_z0_s0
+                else:
+                    front_prob = prob_z0_s1
+
+            # Calculations for right of current cell
+            if right_reading == 1:
+                map_right = grid_map[cell][(CURRENT_DIRECTION+1)%4]
+                if map_right == 'W':
+                    right_prob = prob_z1_s1
+                else:
+                    right_prob = prob_z1_s0
+            elif right_reading == 0:
+                map_right = grid_map[cell][(CURRENT_DIRECTION+1)%4]
+                if map_right == 'O':
+                    right_prob = prob_z0_s0
+                else:
+                    right_prob = prob_z0_s1
+            
+            # Probabilities from readings
+            arr[row][col] = left_prob * front_prob * right_prob
+
+            # Importance Factor
+            arr[row][col] = arr[row][col] * input_arr[row][col]
+
+            # Adding to sum for normalization
+            arr_sum += arr[row][col]
+
+    # Normalization
+    for row in range(len(arr)):
+        for col in range(len(arr[0])):
+            arr[row][col] = arr[row][col] / arr_sum
+
+    # Resampling
+    for row in range(len(arr)):
+        for col in range(len(arr[0])):
+            arr[row][col] = round(arr[row][col] * PARTICLE_NUM)
+
+    for row in arr:
         print(row)
-    return belief_arr
+
+    return arr
 
 def motion_update(arr:list[list[int]], direction:str):
     for row in range(len(arr)):
@@ -148,45 +211,39 @@ def main():
                     9:(-1.5*SQ_SIZE,-0.5*SQ_SIZE), 10:(-0.5*SQ_SIZE,-0.5*SQ_SIZE), 11:(0.5*SQ_SIZE,-0.5*SQ_SIZE), 12:(1.5*SQ_SIZE,-0.5*SQ_SIZE),
                     13:(-1.5*SQ_SIZE,-1.5*SQ_SIZE), 14:(-0.5*SQ_SIZE,-1.5*SQ_SIZE), 15:(0.5*SQ_SIZE,-1.5*SQ_SIZE), 16:(1.5*SQ_SIZE,-1.5*SQ_SIZE)}
 
-    # Map of walls based on cell number and facing north
-    grid_map = {1:'WWOW', 2:'OWOW', 3:'OWOO', 4:'OWWO',
-                5:'WWOO', 6:'OWWO', 7:'WOWO', 8:'WOWO',
-                9:'WOWO', 10:'WOOW', 11:'OOWW', 12:'WOWO',
-                13:'WOOW', 14:'OWOW', 15:'OWOW', 16:'OOWW'}
+
 
     arr = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
-    NUM_SQ = ROWS * COLUMNS
-    starting_probability = 1/NUM_SQ
-    starting_direction = directions[1] # North
-    #print(starting_possibility)
-    #print(cell_num.keys())
+    SQ_NUM = ROWS * COLUMNS
+    starting_particles = PARTICLE_NUM / SQ_NUM
     for i in range(len(arr)):
         for j in range(len(arr[0])):
             #print((i,j),cell_num.get((i,j)))
             #temp_cell_num = cell_num.get((i,j))
             #print(grid_map.get(temp_cell_num))
-            arr[i][j]=starting_probability
+            arr[i][j]=starting_particles
+    '''for row in arr:
+        print(row)'''
 
-    motion_updated_arr = motion_update(copy.deepcopy(arr),starting_direction)
+    readings = [0,1,0]
+    sensor_updated = sensor_update(readings,arr)
+    #motion_updated_arr = motion_update(copy.deepcopy(arr),directions[CURRENT_DIRECTION])
+    #print(motion_updated_arr[row])
+    '''for column in range(len(arr[0])):
+        #print(arr[row][column])
+        pass'''
 
-    for row in range(len(motion_updated_arr)):
-        #print(arr[row])
-        #print(motion_updated_arr[row])
-        '''for column in range(len(arr[0])):
-            #print(arr[row][column])
-            pass'''
-
-    belief_bar_arr = [[0]*COLUMNS]*ROWS
+    '''belief_bar_arr = [[0]*COLUMNS]*ROWS
     for i in range(len(belief_bar_arr)):
         for j in range(len(belief_bar_arr)):
             belief_bar_arr[i][j] = arr[i][j] * motion_updated_arr[i][j]
         #print(belief_bar_arr[i])
 
-    belief_arr = sensor_update(1,1,0,belief_bar_arr,starting_direction)
+    belief_arr = sensor_update(1,1,0,belief_bar_arr,starting_direction)'''
     '''for row in belief_arr:
         print(row)'''
-    
+
     '''basic wall following using lab 3 task 4 code, then use encoders to measure each 18 inches, robot should stop when it
     it reaches a certain amount of ticks, use right wheel ticks as they are more constant'''
 if __name__ == '__main__':
