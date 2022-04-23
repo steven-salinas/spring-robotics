10245f9b613abb029721a5a2dd917cded731bb7d
steven salinas <stevensalinas@live.com>
2022-04-23 14:42:58 -0400

    Added logic for localization

--- Lab_5/task2.py
+++ Lab_5/task2.py
@@ -1,5 +1,4 @@
 import copy
-from lib2to3.pgen2.token import PERCENT
 import time
 
 '''
@@ -40,14 +39,6 @@ index_to_cell = {(0,0):1,(0,1):2,(0,2):3,(0,3):4,
                 (2,0):9,(2,1):10,(2,2):11,(2,3):12,
                 (3,0):13,(3,1):14,(3,2):15,(3,3):16}
 #print(index_to_cell[0,1])
-surrounded_squares = {1:['W']}
-adjacent_squares = {1:['N','S'],4:['N','E'],5:['N','W'],6:['N','E'],10:['W','S'],11:['E','S']
-                ,13:['W','S'],16:['E','S']}
-opposite_squares = {1:['E'],2:['E','W'],7:['N','S'],8:['N','S'],9:['N','S'],12:['N','S']
-                ,14:['E','W'],15:['E','W']}
-single_square = {2:['N','S'],3:['N'],4:['W'],5:['S'],6:['S'],7:['E','W'],8:['E','W']
-                ,9:['E','W'],10:['E'],11:['W'],12:['E','W']
-                ,13:['E'],14:['N','S'],15:['N','S'],16:['W']}
 
 # Map of walls based on cell number and facing north
 grid_map = {1:'WWOW', 2:'OWOW', 3:'OWOO', 4:'OWWO',
@@ -67,11 +58,20 @@ prob_z0_s1 = 0.1
 prob_z1_s0 = 0.3
 prob_z1_s1 = 0.9
 
-# 0 = west, 1 = north, 2 = east, 3 = south
-CURRENT_DIRECTION = 1
-
 # Size of grid square in inches
 SQ_SIZE = 18
+
+# Cell number based on midpoint of cell coordinates
+coord_to_cell = {(-1.5*SQ_SIZE,1.5*SQ_SIZE):1, (-0.5*SQ_SIZE,1.5*SQ_SIZE):2, (0.5*SQ_SIZE,1.5*SQ_SIZE):3, (1.5*SQ_SIZE,1.5*SQ_SIZE):4,
+            (-1.5*SQ_SIZE,0.5*SQ_SIZE):5, (-0.5*SQ_SIZE,0.5*SQ_SIZE):6, (0.5*SQ_SIZE,0.5*SQ_SIZE):7, (1.5*SQ_SIZE,0.5*SQ_SIZE):8,
+            (-1.5*SQ_SIZE,-0.5*SQ_SIZE):9, (-0.5*SQ_SIZE,-0.5*SQ_SIZE):10, (0.5*SQ_SIZE,-0.5*SQ_SIZE):11, (1.5*SQ_SIZE,-0.5*SQ_SIZE):12,
+            (-1.5*SQ_SIZE,-1.5*SQ_SIZE):13, (-0.5*SQ_SIZE,-1.5*SQ_SIZE):14, (0.5*SQ_SIZE,-1.5*SQ_SIZE):15, (1.5*SQ_SIZE,-1.5*SQ_SIZE):16}
+
+# Coordinates based on cell number
+cell_to_coord = {1:(-1.5*SQ_SIZE,1.5*SQ_SIZE), 2:(-0.5*SQ_SIZE,1.5*SQ_SIZE), 3:(0.5*SQ_SIZE,1.5*SQ_SIZE), 4:(1.5*SQ_SIZE,1.5*SQ_SIZE),
+                5:(-1.5*SQ_SIZE,0.5*SQ_SIZE), 6:(-0.5*SQ_SIZE,0.5*SQ_SIZE), 7:(0.5*SQ_SIZE,0.5*SQ_SIZE), 8:(1.5*SQ_SIZE,0.5*SQ_SIZE),
+                9:(-1.5*SQ_SIZE,-0.5*SQ_SIZE), 10:(-0.5*SQ_SIZE,-0.5*SQ_SIZE), 11:(0.5*SQ_SIZE,-0.5*SQ_SIZE), 12:(1.5*SQ_SIZE,-0.5*SQ_SIZE),
+                13:(-1.5*SQ_SIZE,-1.5*SQ_SIZE), 14:(-0.5*SQ_SIZE,-1.5*SQ_SIZE), 15:(0.5*SQ_SIZE,-1.5*SQ_SIZE), 16:(1.5*SQ_SIZE,-1.5*SQ_SIZE)}
 
 # Number of beginning particles
 PARTICLE_PERCENT = 0.8
@@ -80,6 +80,9 @@ PARTICLE_THRESHOLD = PARTICLE_PERCENT * PARTICLE_NUM
 
 ROWS = 4
 COLUMNS = 4
+
+# 0 = west, 1 = north, 2 = east, 3 = south
+CURRENT_DIRECTION = 1
 
 def sensor_update(readings,input_arr:list[list[int]]) -> list[list[int]]:
     left_reading, front_reading, right_reading = readings[0],readings[1], readings[2]
@@ -150,6 +153,7 @@ def sensor_update(readings,input_arr:list[list[int]]) -> list[list[int]]:
 
     current_particles = 0
     global PARTICLE_NUM
+    global PARTICLE_THRESHOLD
     # Resampling
     for row in range(len(arr)):
         for col in range(len(arr[0])):
@@ -165,6 +169,16 @@ def sensor_update(readings,input_arr:list[list[int]]) -> list[list[int]]:
         print(row)
 
     return arr
+
+def get_max_particle(input_arr):
+    max_particle = -1
+    max_idx = (-1,-1)
+    for row in range(len(input_arr)):
+        for col in range(len(input_arr[0])):
+            if input_arr[row][col] > max_particle:
+                max_particle = input_arr[row][col]
+                max_idx = (row,col)
+    return max_idx
 
 def motion_update(input_arr):
     global PARTICLE_NUM
@@ -252,113 +266,92 @@ def motion_update(input_arr):
 def main():
     global CURRENT_DIRECTION
     global PARTICLE_THRESHOLD
-    # Cell number based on midpoint of cell coordinates
-    coord_to_cell = {(-1.5*SQ_SIZE,1.5*SQ_SIZE):1, (-0.5*SQ_SIZE,1.5*SQ_SIZE):2, (0.5*SQ_SIZE,1.5*SQ_SIZE):3, (1.5*SQ_SIZE,1.5*SQ_SIZE):4,
-                (-1.5*SQ_SIZE,0.5*SQ_SIZE):5, (-0.5*SQ_SIZE,0.5*SQ_SIZE):6, (0.5*SQ_SIZE,0.5*SQ_SIZE):7, (1.5*SQ_SIZE,0.5*SQ_SIZE):8,
-                (-1.5*SQ_SIZE,-0.5*SQ_SIZE):9, (-0.5*SQ_SIZE,-0.5*SQ_SIZE):10, (0.5*SQ_SIZE,-0.5*SQ_SIZE):11, (1.5*SQ_SIZE,-0.5*SQ_SIZE):12,
-                (-1.5*SQ_SIZE,-1.5*SQ_SIZE):13, (-0.5*SQ_SIZE,-1.5*SQ_SIZE):14, (0.5*SQ_SIZE,-1.5*SQ_SIZE):15, (1.5*SQ_SIZE,-1.5*SQ_SIZE):16}
-
-    # Coordinates based on cell number
-    cell_to_coord = {1:(-1.5*SQ_SIZE,1.5*SQ_SIZE), 2:(-0.5*SQ_SIZE,1.5*SQ_SIZE), 3:(0.5*SQ_SIZE,1.5*SQ_SIZE), 4:(1.5*SQ_SIZE,1.5*SQ_SIZE),
-                    5:(-1.5*SQ_SIZE,0.5*SQ_SIZE), 6:(-0.5*SQ_SIZE,0.5*SQ_SIZE), 7:(0.5*SQ_SIZE,0.5*SQ_SIZE), 8:(1.5*SQ_SIZE,0.5*SQ_SIZE),
-                    9:(-1.5*SQ_SIZE,-0.5*SQ_SIZE), 10:(-0.5*SQ_SIZE,-0.5*SQ_SIZE), 11:(0.5*SQ_SIZE,-0.5*SQ_SIZE), 12:(1.5*SQ_SIZE,-0.5*SQ_SIZE),
-                    13:(-1.5*SQ_SIZE,-1.5*SQ_SIZE), 14:(-0.5*SQ_SIZE,-1.5*SQ_SIZE), 15:(0.5*SQ_SIZE,-1.5*SQ_SIZE), 16:(1.5*SQ_SIZE,-1.5*SQ_SIZE)}
-
-
 
     arr = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    output_arr = [['0','0','0','0'],['0','0','0','0'],['0','0','0','0'],['0','0','0','0']]
+    max_particle = -1
 
     SQ_NUM = ROWS * COLUMNS
     starting_particles = PARTICLE_NUM / SQ_NUM
     for i in range(len(arr)):
         for j in range(len(arr[0])):
-            #print((i,j),cell_num.get((i,j)))
-            #temp_cell_num = cell_num.get((i,j))
-            #print(grid_map.get(temp_cell_num))
             arr[i][j]=starting_particles
-    '''for row in arr:
-        print(row)'''
-
-    # Simualated reading from sensor
-    # Simulating on map from lab 5
-    # Simulating going from cell 12 up and left to cell 1
-    # Then turning around back to cell 4
-
-    # Cell 12
+    
+    # Cell 12 or with real robot the first reading
     readings = [1,0,1]
     sensor_updated = sensor_update(readings,arr)
+    max_idx = get_max_particle(sensor_updated)
+    max_particle = sensor_updated[max_idx[0]][max_idx[1]]
+    print(max_particle, max_idx)
     motion_updated = motion_update(sensor_updated)
 
-    # 8
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
+    while max_particle < PARTICLE_THRESHOLD:        
+        '''
+        readings = get_readings()
+        if readings[1] == 1:
+            sensor_updated = sensor_update(readings,motion_updated)
+            max_idx = get_max_particle(sensor_updated)
+            max_particle = sensor_updated[max_idx[0]][max_idx[1]]
 
-    # 4
-    readings = [0,1,1]
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
+            motion_updated = motion_update(sensor_updated)
+            turn(90)
+            CURRENT_DIRECTION = (CURRENT_DIRECTION + 1) % 4
+        else:
+            moveXV(18)
+            sensor_updated = sensor_update(readings,motion_updated)
+            max_idx = get_max_particle(sensor_updated)
+            max_particle = sensor_updated[max_idx[0]][max_idx[1]]
 
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
+            motion_updated = motion_update(sensor_updated)
+        '''
 
-    CURRENT_DIRECTION = 0
-    readings = [0,0,1]
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
+        '''Simulating input from going from cell 12 up to cell 4,
+        notice how cell 4 has two readings, as max particle reading is taken
+        before turning 90 degrees clockwise'''
+        # Cell 12
+        readings = [1,0,1]
+        sensor_updated = sensor_update(readings,arr)
+        max_idx = get_max_particle(sensor_updated)
+        max_particle = sensor_updated[max_idx[0]][max_idx[1]]
+        motion_updated = motion_update(sensor_updated)
+        print(max_particle, max_idx)
 
-    readings = [1,0,1]
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
+        # 8
+        sensor_updated = sensor_update(readings,motion_updated)
+        motion_updated = motion_update(sensor_updated)
+        max_idx = get_max_particle(sensor_updated)
+        max_particle = sensor_updated[max_idx[0]][max_idx[1]]
+        print(max_particle, max_idx)
 
-    readings = [1,1,1]
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
+        # 4 taken before turning 90 clockwise
+        readings = [0,1,1]
+        sensor_updated = sensor_update(readings,motion_updated)
+        max_idx = get_max_particle(sensor_updated)
+        max_particle = sensor_updated[max_idx[0]][max_idx[1]]
+        motion_updated = motion_update(sensor_updated)
+        # Turn(90)
+        print(max_particle, max_idx)
+        CURRENT_DIRECTION = CURRENT_DIRECTION = (CURRENT_DIRECTION + 1) % 4
 
-    # This shows that the sensor should also update when there is a wall in front and before turning
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
+        # 4 taken before turning 90 clockwise
+        readings = [1,1,0]
+        sensor_updated = sensor_update(readings,motion_updated)
+        max_idx = get_max_particle(sensor_updated)
+        max_particle = sensor_updated[max_idx[0]][max_idx[1]]
+        motion_updated = motion_update(sensor_updated)
+        
+        print(max_particle, max_idx)
 
-    # Simulated right turn here to face another wall
+        print("END LOCALIZATION HERE")
 
-    # Another reading before taking right turn
-    readings = [1,1,0]
-    CURRENT_DIRECTION = 1
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
+    localized_cell = index_to_cell[max_idx]
+    # + "going" '''+ directions[CURRENT_DIRECTION]'''
+    print("Robot is at: {} going {}".format(cell_to_coord[localized_cell], directions[CURRENT_DIRECTION]))
 
-    # Simulated right turn here
-
-    # Readings before  continuing forward, still in cell 1
-    readings = [1,0,1]
-    CURRENT_DIRECTION = 2
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
-
-    # At cell 2
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
-
-    # At cell 3
-    readings = [1,0,0]
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
-
-    # At cell 4
-    readings = [1,1,0]
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
-    print(PARTICLE_THRESHOLD)
-
-    # Same as before, if wall in front do another resampling before turning
-    sensor_updated = sensor_update(readings,motion_updated)
-    motion_updated = motion_update(sensor_updated)
-    print(PARTICLE_THRESHOLD)
-    
     '''TODO
-    - Add motion capabilities to move one square
-    - Detect when sensor_updated has a max cell over threshold
-    - Add funtionality to turn right when wall in front and do resampling beforehand
-    - Once localized should be able to output current square as well as coordinates
     - Naviagate around the rest of the squares once localized'''
+    output_arr[max_idx[0]][max_idx[1]] = 'X'
+    for row in output_arr:
+        print(row)
 if __name__ == '__main__':
     main()
