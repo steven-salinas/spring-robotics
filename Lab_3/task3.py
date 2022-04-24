41e401a8566dfa2a45931be23225bcddeb5a8e16
steven salinas <stevensalinas@live.com>
2022-04-24 15:16:07 -0400

    Changed to correct lab 3 files

---
+++ Lab_3/task3.py
@@ -0,0 +1,126 @@
+import utils
+import time
+
+import signal
+import math
+import RPi.GPIO as GPIO # README: https://pypi.org/project/RPi.GPIO/
+
+import PID
+import threading
+
+
+def ctrlC(signum, frame):
+    global breakFlag
+    breakFlag=True
+    print("Ctrl-C caught")
+
+    # threadPID.join()
+    print("Exiting")
+
+    lSensor.stop_ranging()
+    fSensor.stop_ranging()
+    rSensor.stop_ranging()
+    utils.setSpeedsPWM (1.508,1.5)
+    GPIO.cleanup()
+    exit()
+
+
+
+
+
+
+if __name__ == "__main__":
+    global breakFlag
+    global pidL, pidR
+    global lSensor, fSensor, rSensor
+    global startSpeedL, startSpeedR
+
+
+
+
+    breakFlag=False
+    signal.signal(signal.SIGINT, ctrlC)
+
+    #initialize motors/encoders, PID is initialized with values inputted
+    utils.initEncoders()
+    utils.initMotors()
+    utils.initPID(0,0,0)
+    # imu=utils.initIMU()
+    utils.initIMU()
+    utils.initTOF()
+
+
+
+    #Grab initialized objects from utils
+    pidLWall=PID.PID(1,0,0)
+
+
+    lSensor=utils.lSensor
+    fSensor=utils.fSensor
+    rSensor=utils.rSensor
+
+    #Getting PID thread ready
+
+
+
+
+    print("Starting")
+
+
+
+
+    #Body code here-start
+
+
+
+    startSpeedL=2
+    startSpeedR=2
+
+
+    pidLWall.SetPoint=7
+
+
+
+    updateRate=0.1
+    mmToInch=0.0393701
+
+    fCount=0
+    startTime=time.time()
+    while time.time()-startTime<160:
+
+
+
+        fDistance = fSensor.get_distance()*mmToInch
+        lDistance = lSensor.get_distance()*mmToInch
+        rDistance = rSensor.get_distance()*mmToInch
+        timeElapsed=time.time()-startTime
+        timeElapsed=round(timeElapsed,2)
+        print("Left: {} Right: {} Front: {} Time Elapsed: {}".format(round(lDistance,2),round(rDistance,2),round(fDistance,2),timeElapsed))
+
+        #print(fDistance,lDistance)
+        if fDistance<7:
+
+            utils.rotateA(90)
+
+
+        else:
+
+
+            # if lDistance
+            fCount=0
+            pidLWall.update(lDistance)
+            setSpeedL=pidLWall.output+startSpeedL
+            #print(pidLWall.SetPoint,lDistance,pidLWall.output,setSpeedL)
+            setSpeedL=utils.clamp(setSpeedL,-3,3)
+            utils.setSpeedsIPS(setSpeedL,startSpeedR,False)
+            time.sleep(updateRate)
+
+
+    #Body code here -end
+
+    #Cleanup
+
+    print("Exiting")
+    utils.setSpeedsPWM (1.508,1.5)
+    GPIO.cleanup()
+    exit()
--- Lab_3/utils.py
+++ Lab_3/utils.py
@@ -1,7 +1,3 @@
-'''
-    utils.py contains simple functions utilized to form more
-    complex manuever for robot.
-'''
 import time
 import Adafruit_PCA9685
 import signal
@@ -15,6 +11,10 @@ import PID
 import threading
 import board
 import adafruit_bno055
+import sys
+sys.path.append('/home/pi/VL53L0X_rasp_python/python')
+import VL53L0X
+
 
 # The servo hat uses its own numbering scheme within the Adafruit library.
 # 0 represents the first servo, 1 for the second, and so on.
@@ -27,18 +27,6 @@ RENCODER = 18
 setSpeedL=0
 setSpeedR=0
 
-wheel_diam=1.3 #Wheel diameter in inches
-
-maxIPS=5
-maxRPS=maxIPS/(2*math.pi*wheel_diam)
-
-
-pwmMapL=[1.448,1.453,1.458,1.463,1.468,1.473,1.478,1.483,1.488,1.493,1.498,1.503,1.508,1.513,1.518,1.523,1.528,1.533,1.538,1.543,1.548,1.553,1.558,1.563]
-ipsMapL=[-4.65,-4.2,-3.75,-3.3,-2.85,-2.4,-1.94,-1.583,-1.123,-0.638,-0.153,0,0,0.1786,0.63,1.123,1.601,2.075,2.45,2.9,3.35,3.8,4.25,4.7]
-
-pwmMapR=[1.443,1.448,1.453,1.458,1.463,1.468,1.473,1.478,1.483,1.488,1.493,1.498,1.503,1.508,1.513,1.518,1.523,1.528,1.533,1.538,1.543,1.548,1.553,1.558,1.563]
-ipsMapR=[-4.65,-4.2,-3.75,-3.3,-2.85,-2.4,-1.94,-1.48,-1.05,-0.613,-0.077,0,0,0.08,0.48,0.89,1.4,1.813,2.25,2.7,3.15,3.6,4.05,4.5,4.95]
-
 global pidL, pidR
 def initPID(P,I,D):
     global pidL, pidR
@@ -48,6 +36,51 @@ def initPID(P,I,D):
     pidL.SetPoint=0
     pidR.SetPoint=0
     # return pidL, pidR
+
+def initTOF():
+    global lSensor, fSensor, rSensor
+    LSHDN = 27
+    FSHDN = 22
+    RSHDN = 23
+
+    DEFAULTADDR = 0x29 # All sensors use this address by default, don't change this
+    LADDR = 0x2a
+    RADDR = 0x2b
+
+    # Set the pin numbering scheme to the numbering shown on the robot itself.
+    GPIO.setmode(GPIO.BCM)
+
+    # Setup pins
+    GPIO.setup(LSHDN, GPIO.OUT)
+    GPIO.setup(FSHDN, GPIO.OUT)
+    GPIO.setup(RSHDN, GPIO.OUT)
+
+    # Shutdown all sensors
+    GPIO.output(LSHDN, GPIO.LOW)
+    GPIO.output(FSHDN, GPIO.LOW)
+    GPIO.output(RSHDN, GPIO.LOW)
+
+    time.sleep(0.01)
+
+    # Initialize all sensors
+    lSensor = VL53L0X.VL53L0X(address=LADDR)
+    fSensor = VL53L0X.VL53L0X(address=DEFAULTADDR)
+    rSensor = VL53L0X.VL53L0X(address=RADDR)
+
+    # Connect the left sensor and start measurement
+    GPIO.output(LSHDN, GPIO.HIGH)
+    time.sleep(0.01)
+    lSensor.start_ranging(VL53L0X.VL53L0X_GOOD_ACCURACY_MODE)
+
+    # Connect the right sensor and start measurement
+    GPIO.output(RSHDN, GPIO.HIGH)
+    time.sleep(0.01)
+    rSensor.start_ranging(VL53L0X.VL53L0X_GOOD_ACCURACY_MODE)
+
+    # Connect the front sensor and start measurement
+    GPIO.output(FSHDN, GPIO.HIGH)
+    time.sleep(0.01)
+    fSensor.start_ranging(VL53L0X.VL53L0X_GOOD_ACCURACY_MODE)
 
 def initIMU():
     i2c = board.I2C()
@@ -61,7 +94,7 @@ def initMotors():
 
     # 50Hz is used for the frequency of the servos.
     pwm.set_pwm_freq(50)
-    setSpeedsPWM (1.5,1.495)
+    #setSpeedsPWM (1.5,1.495)
     return pwm
 
 def remap(value, fromLow, fromHigh, toLow, toHigh):
@@ -74,15 +107,13 @@ def remap(value, fromLow, fromHigh, toLow, toHigh):
 def clamp(value, low, high):
     return max(min(high, value), low)
 
+
 def closest(lst, K):
 
      lst = np.asarray(lst)
      idx = (np.abs(lst - K)).argmin()
      return idx
 
-# Function to move robot X inches at given velocity V
-# X - Distance to move in inches
-# V - Velocity to move at in inches/second
 def moveXV(X,V):
     global maxIPS
 
@@ -117,14 +148,22 @@ def moveXV(X,V):
         print("Requested velocity is not able to be set, movement cancelled\n")
         setSpeedsPWM (1.5,1.495)
 
-# Set the speeds of the servo motors by giving
-# width of pulse modulation.
-# pwmLeft - pulse width for left motor
-# pwmRight - pulse width for right motor
-# 
-# Note - motors are mounted in opposite directions
+wheel_diam=1.3 #Wheel diameter in inches
+
+maxIPS=5
+maxRPS=maxIPS/(2*math.pi*wheel_diam)
+
+
+pwmMapL=[1.448,1.453,1.458,1.463,1.468,1.473,1.478,1.483,1.488,1.493,1.498,1.503,1.508,1.513,1.518,1.523,1.528,1.533,1.538,1.543,1.548,1.553,1.558,1.563]
+ipsMapL=[-4.65,-4.2,-3.75,-3.3,-2.85,-2.4,-1.94,-1.583,-1.123,-0.638,-0.153,0,0,0.1786,0.63,1.123,1.601,2.075,2.45,2.9,3.35,3.8,4.25,4.7]
+
+pwmMapR=[1.443,1.448,1.453,1.458,1.463,1.468,1.473,1.478,1.483,1.488,1.493,1.498,1.503,1.508,1.513,1.518,1.523,1.528,1.533,1.538,1.543,1.548,1.553,1.558,1.563]
+ipsMapR=[-4.65,-4.2,-3.75,-3.3,-2.85,-2.4,-1.94,-1.48,-1.05,-0.613,-0.077,0,0,0.08,0.48,0.89,1.4,1.813,2.25,2.7,3.15,3.6,4.05,4.5,4.95]
+
+
 def setSpeedsPWM (pwmLeft, pwmRight):
     global pwmLeftCurrent, pwmRightCurrent
+
 
     Lcal=0
     Rcal=0
@@ -140,12 +179,12 @@ def setSpeedsPWM (pwmLeft, pwmRight):
         pwmLeftCurrent=pwmLeft
         pwmRightCurrent=pwmRight
         #print("Setting motor speed in PWM, left: {} right: {}".format(pwmLeft,pwmRight))
-        pwm.set_pwm(LSERVO, 0, math.floor(pwmLeft / 20 * 4096))
-        pwm.set_pwm(RSERVO, 0, math.floor(pwmRight / 20 * 4096))
+        pwm.set_pwm(LSERVO, 0, math.floor(pwmLeft / 20 * 4096));
+        pwm.set_pwm(RSERVO, 0, math.floor(pwmRight / 20 * 4096));
 
-# Set robot speed in rotation per second of wheels
-# rpsLeft - rotations per second of left wheel
-# rpsRight - rotations per second of right wheel
+
+
+
 def setSpeedsRPS (rpsLeft, rpsRight):
     global maxRPS
     global wheel_diam
@@ -158,12 +197,7 @@ def setSpeedsRPS (rpsLeft, rpsRight):
         setSpeedsIPS(ipsLeft,ipsRight)
 
 
-# Function to move robot at a given left and right linear velocity.
-# This function uses the PID and is used by other functions as a 
-# function to utilize PID control.
-# ipsLeft - left wheel linear velocity in inches/second
-# ipsLeft - left wheel linear velocity in inches/second
-# setPID - boolean to choose whether or not PID control is used
+
 def setSpeedsIPS(ipsLeft, ipsRight,setPID=True):
     global maxIPS
 
@@ -190,6 +224,8 @@ def setSpeedsIPS(ipsLeft, ipsRight,setPID=True):
         pidL.SetPoint=ipsLeft
         pidR.SetPoint=ipsRight
         # print(pidL.SetPoint)
+
+
 
     if abs(ipsLeft)>maxIPS or abs(ipsRight)>maxIPS:
         print("Not able to set motor speed to IPS, left: {} right: {}".format(ipsLeft,ipsRight))
@@ -223,12 +259,11 @@ def setSpeedsIPS(ipsLeft, ipsRight,setPID=True):
         pwmR=pwmMapR[idxR]+interR
         pwmR=round(pwmR,4)
 
+
         #print(pwmL,pwmR)
         setSpeedsPWM(pwmL,pwmR)
 
-# Function to set robot to move at given linear and angular velocity
-# V - velocity in inches per second
-# W - angular velocity in radians per second
+
 def setSpeedsVW(V, W):
     global maxIPS
     #print("Setting motor speed in VW, V: {} W: {}".format(V,W))
@@ -254,9 +289,8 @@ def setSpeedsVW(V, W):
         print("Motors are not capable of completing maneuver, outer {} inner {}".format(Vl,Vr))
 
 
-'''
-    Encoder functions
-'''
+
+
 left_count=0
 right_count=0
 
@@ -372,8 +406,6 @@ last_val = 0xFFFF
 '''
 	IMU functions
 '''
-
-# Function to return current temperature from IMU sensor
 def temperature():
 	global last_val
 	result = sensor.temperature
@@ -384,33 +416,40 @@ def temperature():
 		last_val = result
 	return result
 
-# Function to turn robot angle degrees using IMU euler angle
-# Function should stop turning robot after turing angle degrees clockwise
-# angle - angle in degrees of desired robot rotation
+# Function to turn robot 90 degrees using IMU euler angle
+# Function should stop turning robot after turing 90 degrees clockwise
 #
-# Note - There is a small amount of inconsistency
+# Note - However there is a small amount of inconsistency
 #        due to not being able to call euler position frequently
-#        enough without erroneous response.
+#        enough without erroneous response
 def rotateA(angle):
     global pidL,pidR
-
+    print("Rotating {} degrees".format(angle))
     setSpeedsIPS(0,0)
+
 
     pidL.enable=False
     pidR.enable=False
 
-    speed=1
+    pidIMU=PID.imuPID(0.5,0,0)
+
+    speed=2
 
     angleResolution=1
 
+    # angle=angle % 360
     angleCurrent = sensor.euler[0]
     angleGoal=(angleCurrent+angle)%360
 
     angleError= (angleCurrent - angleGoal + 540)%360 - 180
 
-    # Loop will repeat until less than angle of resolution
+    initUpdateTime=(2*math.pi*2*5/360)/speed
     while abs(angleError)>angleResolution:
 
+        pidIMU.update(angleError)
+        output=abs(pidIMU.output)
+
+        updateTime=(2*math.pi*2*output/360)/speed
         if angleError>0: #if error is positive turn left
 
             setSpeedsIPS(-speed,speed,False)
@@ -418,6 +457,9 @@ def rotateA(angle):
         else: #if negative turn right
             setSpeedsIPS(speed,-speed,False)
 
+        time.sleep(updateTime)
+        setSpeedsIPS(0,0,False)
+        time.sleep(0.001)
         angleCurrent = sensor.euler[0] if sensor.euler[0] is not None else angleCurrent
         angleError= (angleCurrent - angleGoal + 540)%360 - 180
         # time.sleep(.1)
@@ -428,15 +470,13 @@ def rotateA(angle):
             # print(int(angleErrorAvg))
 
     setSpeedsIPS(0,0)
-
+    time.sleep(0.1)
     angleCurrent = sensor.euler[0] if sensor.euler[0] is not None else angleCurrent
     angleError= (angleCurrent - angleGoal + 540)%360 - 180
     print("Finished rotating {} deg with {} deg of error".format(angle,angleError))
     pidL.enable=True
     pidR.enable=True
 
-# Function to return the current x Euler angle from 
-# BNO055 IMU, range of values = [0,360].
 lastIMU=0
 def getIMUDegrees():
     global lastIMU
